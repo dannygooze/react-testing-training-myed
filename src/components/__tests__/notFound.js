5ca893e8fdcb6b7dc34bbb1e183fb885ff5af597
Danny Guzman <dannygooze93@gmail.com>
2020-05-14 13:30:15 -0700

    Add tests for not found component

---
+++ src/components/__tests__/notFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { NotFound } from '../notFound'
+
+test('Test NotFound component renders', () => {
+    const { getByText } = render(<NotFound />)
+
+    expect(getByText(/Data was not found in Pokédex/))
+})
