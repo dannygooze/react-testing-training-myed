307ad3c046fb99388670df944076c9b843d7fda1
Danny Guzman <dannygooze93@gmail.com>
2020-05-22 14:15:23 -0700

    Delete tests from master so other can branch off and write their own tests.

--- src/components/__tests__/notFound.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import { NotFound } from '../notFound'
-
-test('Test NotFound component renders', () => {
-    const { getByText } = render(<NotFound />)
-
-    expect(getByText('Data was not found in Pokédex'))
-})
